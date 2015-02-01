54c2c9211fc3e431ef9831db30480b6ea5a1b0e9
深川潤也 <fjunya@fukagawauruunari-no-MacBook-Pro.local>
2015-02-01 18:33:45 +0900

    wsgi.pyの修正

--- src/DjangoTest/DjangoTest/wsgi.py
+++ src/DjangoTest/DjangoTest/wsgi.py
@@ -8,7 +8,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 sys.path.append('/home/fjunya/django/DjangoTest/src')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoTest.DjangoTest.settings")
 
